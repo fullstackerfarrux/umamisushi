10f45884d69e82789db6528d1fc9b092094d9450
farrux <orziyevfarrux01@gmail.com>
2023-08-31 03:08:23 +0500

    changed slider

--- src/Components/Slider.jsx
+++ src/Components/Slider.jsx
@@ -1,19 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import useEmblaCarousel from "embla-carousel-react";
 import Autoplay from "embla-carousel-autoplay";
 
 const Slider = () => {
   const [emblaRef] = useEmblaCarousel({ loop: true }, [Autoplay()]);
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    async function get() {
+      await fetch("https://api.umamisushibot.uz/banner/get", {
+        method: "GET",
+      })
+        .then((res) => res.json())
+        .then((data) => setImages(data.images));
+    }
+
+    get();
+  }, []);
 
   return (
     <div className="container embla" ref={emblaRef}>
       <div className="embla__container">
-        <img
-          src="./bannerumami.png"
-          className="embla__slide"
-          width={350}
-          height={140}
-        />
+        {images?.map((p, index) => (
+          <img
+            src={p.banner_img}
+            className="embla__slide"
+            width={350}
+            height={140}
+            key={index}
+          />
+        ))}
       </div>
     </div>
   );
